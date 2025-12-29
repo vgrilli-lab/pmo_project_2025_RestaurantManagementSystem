e2655ad6b8ae11593a7b5af087c359d4a88c7bef
annalitvinenkouniurb <a.litvinenko@campus.uniurb.it>
2025-12-29 12:09:38 +0100

    1st architecture version: main classes templates added

---
+++ src/it/uniurb/RestaurantMS/RestaurantMS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it.uniurb.RestaurantMS;
+
+/**
+ *
+ * @author lucapaolillo
+ */
+public class RestaurantMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/it/uniurb/RestaurantMS/model/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.uniurb.RestaurantMS.model;
+// stupidina chi legge :D
+/**
+ *
+ * @author lucapaolillo
+ */
+public class User {
+    
+}
